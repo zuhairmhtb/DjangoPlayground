b19be86300e3bc99baa5280f14a144fc61db7fed
zuhairmhtb <zuhairmhtb@gmail.com>
2024-11-01 00:54:27 +0600

    Added debug config in manage.py

--- app/manage.py
+++ app/manage.py
@@ -2,11 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.conf import settings
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_playground.settings')
+    # if settings.DEBUG:
+    #     if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+    #         import debugpy
+    #         debugpy.listen(("0.0.0.0", 4000))
+    #         debugpy.wait_for_client()
+    #         print('Attached!')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
