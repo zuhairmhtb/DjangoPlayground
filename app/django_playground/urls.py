1106481b6a0bb512cd0cdd107490a9c67732e70c
zuhairmhtb <zuhairmhtb@gmail.com>
2024-11-01 01:42:27 +0600

    Added GraphQL schema and support

--- app/django_playground/urls.py
+++ app/django_playground/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from strawberry.django.views import AsyncGraphQLView
+
+from django_playground.schema import schema
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("graphql/", AsyncGraphQLView.as_view(schema=schema)),
 ]
